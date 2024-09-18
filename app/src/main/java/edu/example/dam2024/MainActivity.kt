986860a219a9743afffd32f4445977010db3d1f6
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-09-18 16:58:59 +0200

    first explication of Kotlin

--- app/src/main/java/edu/example/dam2024/MainActivity.kt
+++ app/src/main/java/edu/example/dam2024/MainActivity.kt
@@ -6,15 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+private var name: String? = null
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
+
+/**  Conceptos y cosas importantes
+ *
+ *
+ *
+ *
+ */
