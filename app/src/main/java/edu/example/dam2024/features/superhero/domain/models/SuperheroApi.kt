1d7bdff5662783aa222e7e97cc943663088aea2a
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2024-09-24 00:58:35 +0200

    Añadida funcionalidad para obtener una película mediante el id

--- app/src/main/java/edu/example/dam2024/features/superhero/domain/models/SuperheroApi.kt
+++ app/src/main/java/edu/example/dam2024/features/superhero/domain/models/SuperheroApi.kt
@@ -1,7 +1,8 @@
 package edu.example.dam2024.features.superhero.domain.models
 
-data class SuperheroApi(
+ class SuperheroApi
+    /*(
     val id: String, val name: String, val slug: String, val powerstats: PowerStats,
     val appearance: Appearance, val biography: Biography, val work: Work,
-    val connections: Connections, val images: Images) {
+    val connections: Connections, val images: Images) */{
 }
