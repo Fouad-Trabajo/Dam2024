a8c1bf12fea9e76814f0a754c307e9003672540c
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-09-24 17:44:51 +0200

    feat: Devolver la película tras pulsar sobre ella (getMovieUseCase)

--- app/src/main/java/edu/example/dam2024/features/movies/data/remote/MovieMockRemoteDataSource.kt
+++ app/src/main/java/edu/example/dam2024/features/movies/data/remote/MovieMockRemoteDataSource.kt
@@ -8,18 +8,24 @@ import edu.example.dam2024.features.movies.domain.models.Movie
 
 class MovieMockRemoteDataSource {
 
+    private val movies = listOf(
+        Movie("1", "TeneT", "-"),
+        Movie("2", "Seven", "-"),
+        Movie("3", "Fight Club", "-"),
+        Movie("4", "Pulp Fiction", "-")
+        //Movie(title = "title4", poster = "poster4", id = "4")
+    )
 
-    fun getMovies(): List<Movie>{
-        return listOf(
-            Movie("1", "TeneT", "-"),
-            Movie("2", "Seven", "-"),
-            Movie("3", "Fight Club", "-"),
-            Movie(title = "title4", poster = "poster4", id = "4")
-            // En Kotlin puedes alterar el orden del constructor
-        )
+    fun getMovies(): List<Movie> {
+        return movies
+        // En Kotlin puedes alterar el orden del constructor
+
     }
 
     fun getMovie(id: String): Movie? {
-        return getMovies().find { it.id == id }
+        return movies.firstOrNull { movie -> //renobrar el it por movie
+            //it es un objeto Movie del listado
+            movie.id == id
+        } //También se puede hacer con .find { it.id == id }
     }
 }
