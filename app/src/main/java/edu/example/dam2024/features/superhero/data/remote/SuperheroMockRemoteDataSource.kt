3f62a018a917be2bf4d2af0a930d8dd84da3b1d8
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2024-09-24 18:54:26 +0200

    feat: Añadida funcionalidad para mostrar una película clicando en ella en el emulador de Android Studio

--- app/src/main/java/edu/example/dam2024/features/superhero/data/remote/SuperheroMockRemoteDataSource.kt
+++ app/src/main/java/edu/example/dam2024/features/superhero/data/remote/SuperheroMockRemoteDataSource.kt
@@ -4,13 +4,15 @@ import edu.example.dam2024.features.superhero.domain.models.Superhero
 
 class SuperheroMockRemoteDataSource {
 
+    private val superheroes = listOf(
+        Superhero("1", "Batman"),
+        Superhero("2", "Superman"),
+        Superhero("3", "Spider man"),
+        Superhero("4", "Ironman")
+    )
+
     fun getSuperheroes(): List<Superhero> {
-        return listOf(
-            Superhero("1", "Batman"),
-            Superhero("2", "Superman"),
-            Superhero("3", "Spiderman"),
-            Superhero("4", "Ironman")
-        )
+        return superheroes
     }
 
     fun getSuperhero(id: String): Superhero? {
