f1ffce304a8fa75a71ab15ba72c1798ee80d84ff
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-10-03 20:37:31 +0200

    feat: Creada la primera vista a detalla de los superheroes

--- app/src/main/java/edu/example/dam2024/features/superhero/presentation/SuperheroDetailActivity.kt
+++ app/src/main/java/edu/example/dam2024/features/superhero/presentation/SuperheroDetailActivity.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import edu.example.dam2024.R
+import edu.example.dam2024.app.extensions.loadUrl
 import edu.example.dam2024.features.superhero.domain.models.Superhero
 
 class SuperheroDetailActivity : AppCompatActivity() {
@@ -38,8 +40,15 @@ class SuperheroDetailActivity : AppCompatActivity() {
     }
 
     private fun bindData(superhero: Superhero){
-        val imageView = findViewById<ImageView>(R.id.image)
-        Glide.with(this).load(superhero.image).into(imageView)
+        // Texto
+        val idTextView = findViewById<TextView>(R.id.title_1) // Reemplaza con el ID correcto de tu TextView
+        val nameTextView = findViewById<TextView>(R.id.title_1) // Reemplaza con el ID correcto de tu TextView
+        idTextView.text = superhero.id.toString()  // Asegúrate de que id sea un String
+        nameTextView.text = superhero.name
+
+        // Imagen
+        val imageView = findViewById<ImageView>(R.id.image_1)
+        imageView.loadUrl(superhero.image)
     }
 
     //Función estática que siempre va en la parte de abajo de la clase
