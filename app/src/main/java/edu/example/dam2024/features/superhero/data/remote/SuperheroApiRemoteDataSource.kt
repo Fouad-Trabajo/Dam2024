3160df641271a2c21678b84432553aed48a9e9a7
Fouad (Fuaa) Aharchi Aharchi <fouad.ahaaha@educa.jcyl.es>
2024-10-16 01:58:06 +0200

    feat: Ya me funcionan ambas lista de ambos modelos y he conseguido conectar con ambas apis

--- app/src/main/java/edu/example/dam2024/features/superhero/data/remote/SuperheroApiRemoteDataSource.kt
+++ app/src/main/java/edu/example/dam2024/features/superhero/data/remote/SuperheroApiRemoteDataSource.kt
@@ -3,27 +3,18 @@ package edu.example.dam2024.features.superhero.data.remote
 import edu.example.dam2024.features.superhero.data.remote.api.SuperheroApiClient
 import edu.example.dam2024.features.superhero.domain.models.Superhero
 
+
 class SuperheroApiRemoteDataSource {
 
     private val superheroApiClient = SuperheroApiClient()
 
     suspend fun getSuperheroes(): List<Superhero> {
         val response = superheroApiClient.superheroApiService.getSuperheroes()
-        return if (response.isSuccessful) {
-            response.body() ?: emptyList()
-        } else {
-            // Manejo de errores, puedes lanzar una excepción o manejarlo como desees
-            emptyList() // o lanzar una excepción
-        }
+        return response.body() ?: emptyList()
     }
 
     suspend fun getSuperhero(id: String): Superhero? {
         val response = superheroApiClient.superheroApiService.getSuperhero(id)
-        return if (response.isSuccessful) {
-            response.body()
-        } else {
-            // Manejo de errores, puedes lanzar una excepción o manejarlo como desees
-            null // o lanzar una excepción
-        }
+        return response.body()
     }
 }
