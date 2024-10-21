da42164ff7099c4301f82e3432bebe549942317d
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-10-21 16:40:04 +0200

    chore: sobreescritura del método getItemCount en el ViewHolder, además se han solucionado pequeños bugs y renombramiento de iconos

--- app/src/main/java/edu/example/dam2024/features/movies/presentation/adapter/MovieAdapter.kt
+++ app/src/main/java/edu/example/dam2024/features/movies/presentation/adapter/MovieAdapter.kt
@@ -7,19 +7,18 @@ import edu.example.dam2024.R
 import edu.example.dam2024.features.movies.domain.models.Movie
 import edu.example.dam2024.features.movies.presentation.MovieDiffUtil
 
-class MovieAdapter: ListAdapter<Movie, MovieViewHolder>(MovieDiffUtil()) {
+class MovieAdapter : ListAdapter<Movie, MovieViewHolder>(MovieDiffUtil()) {
 
     private lateinit var onClick: (movieId: String) -> Unit
-    fun setEvent(onClick: (movieId: String) -> Unit){
+    fun setEvent(onClick: (movieId: String) -> Unit) {
         this.onClick = onClick
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.view_movie_item,parent,false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.view_movie_item, parent, false)
         return MovieViewHolder(view)
     }
-
-    override fun getItemCount(): Int = currentList.size
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         holder.bind(currentList[position], onClick)
