e116461e5ac111823d2e97fff640f9df9e3a8105
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-10-25 20:31:13 +0200

    chore: Change method `findById` for binding

--- app/src/main/java/edu/example/dam2024/features/superhero/presentation/SuperheroActivity.kt
+++ app/src/main/java/edu/example/dam2024/features/superhero/presentation/SuperheroActivity.kt
@@ -2,19 +2,20 @@ package edu.example.dam2024.features.superhero.presentation
 
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import edu.example.dam2024.R
+import edu.example.dam2024.databinding.ActivitySuperheroesBinding
 
 class SuperheroActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySuperheroesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_superheroes)
+        binding = ActivitySuperheroesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupView()
     }
 
@@ -25,15 +26,12 @@ class SuperheroActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.superhero_container) as NavHostFragment
 
         val navController = navHostFragment.navController
-        findViewById<BottomNavigationView>(R.id.menu_activity_superhero)
-            .setupWithNavController(navController)
+        binding.menuActivitySuperhero.setupWithNavController(navController)
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if (destination.id == R.id.fragment_superheroes || destination.id == R.id.fragment_superhero_2) {
-                findViewById<BottomNavigationView>(R.id.menu_activity_superhero)
-                    .visibility = View.VISIBLE
+                binding.menuActivitySuperhero.visibility = View.VISIBLE
             } else {
-                findViewById<BottomNavigationView>(R.id.menu_activity_superhero)
-                    .visibility = View.GONE
+                binding.menuActivitySuperhero.visibility = View.GONE
             }
         }
     }
