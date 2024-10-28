e77017876afa170b1ea369376e25a9c8b77939af
FOUAAHA <fouad.ahaaha@educa.jcyl.es>
2024-10-28 17:09:27 +0100

    chore: cambiar la forma de acceder a las llamadas de api

--- app/src/main/java/edu/example/dam2024/app/data/api/ApiClient.kt
+++ app/src/main/java/edu/example/dam2024/app/data/api/ApiClient.kt
@@ -7,20 +7,35 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
+    // Define las URL base para tus APIs
     private const val BASE_URL_SUPERHERO = "https://akabab.github.io/superhero-api/api/"
     const val BASE_URL_POKEMON = "https://pokeapi.co/api/v2/"
 
-    private fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder().baseUrl(BASE_URL_SUPERHERO)
-            .addConverterFactory(GsonConverterFactory.create()).build()
-        //Es muy importante añadir el converter
+    // Un mapa para almacenar servicios
+    private val serviceMap = mutableMapOf<String, Any>()
+
+    // Método genérico para crear el cliente Retrofit
+    private fun <T> createService(baseUrl: String, serviceClass: Class<T>): T {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(serviceClass)
     }
 
+    // Método para obtener un servicio, creando uno nuevo si no existe
+    private fun <T : Any> provideService(baseUrl: String, serviceClass: Class<T>): T {
+        return serviceMap[baseUrl] as? T ?: createService(baseUrl, serviceClass).also {
+            serviceMap[baseUrl] = it
+        }
+    }
+
+    // Métodos específicos para obtener tus servicios
     fun provideSuperheroService(): SuperheroApiService {
-        return provideRetrofit().create(SuperheroApiService::class.java)
+        return provideService(BASE_URL_SUPERHERO, SuperheroApiService::class.java)
     }
 
     fun providePokemonService(): PokemonApiService {
-        return provideRetrofit().create(PokemonApiService::class.java)
+        return provideService(BASE_URL_POKEMON, PokemonApiService::class.java)
     }
 }
